47177cbf1a6afc1e444aceaebb1728bd0bf28193
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-08-18 16:28:11 +0200

    Add empty project linked to sql database

---
+++ DB_application/DB_application/Form1.cs
@@ -0,0 +1,32 @@
+﻿using AnimaliContext;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (AnimaliDataContext ctx = new AnimaliDataContext())
+            {
+                var gab1 = new Gabbia();
+                gab1.Lunghezza = "15";
+                ctx.Gabbias.InsertOnSubmit(gab1);
+                ctx.SubmitChanges();
+            }
+        }
+    }
+}
